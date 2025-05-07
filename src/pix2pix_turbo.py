a6199b3598c672275b860d1ec638955fbe70173f
Andreas Jörg <andreasjoerg@04b4540c-785f-4f1b-9101-24e4f955f4b5.fritz.box>
2025-05-07 17:28:31 +0200

    bugfix

--- src/pix2pix_turbo.py
+++ src/pix2pix_turbo.py
@@ -160,6 +160,47 @@ class TwinConv(torch.nn.Module):
         return x1 * (1 - self.r) + x2 * (self.r)
 
 
+def encode_prompt(prompt, tokenizers, text_encoders):
+    prompt_embeds_list = []
+
+    for tokenizer, text_encoder in zip(tokenizers, text_encoders):
+        text_inputs = tokenizer(
+            prompt,
+            padding="max_length",
+            max_length=tokenizer.model_max_length,
+            truncation=True,
+            return_tensors="pt",
+        )
+        text_input_ids = text_inputs.input_ids
+        untruncated_ids = tokenizer(prompt, padding="longest", return_tensors="pt").input_ids
+
+        if untruncated_ids.shape[-1] >= text_input_ids.shape[-1] and not torch.equal(
+            text_input_ids, untruncated_ids
+        ):
+            removed_text = tokenizer.batch_decode(untruncated_ids[:, tokenizer.model_max_length - 1 : -1])
+            logger.warning(
+                "The following part of your input was truncated because CLIP can only handle sequences up to"
+                f" {tokenizer.model_max_length} tokens: {removed_text}"
+            )
+
+        prompt_embeds = text_encoder(
+            text_input_ids.to(text_encoder.device),
+            output_hidden_states=True,
+        )
+
+        # We are only ALWAYS interested in the pooled output of the final text encoder
+        pooled_prompt_embeds = prompt_embeds[0]
+        prompt_embeds = prompt_embeds.hidden_states[-2]
+        bs_embed, seq_len, _ = prompt_embeds.shape
+        prompt_embeds = prompt_embeds.view(bs_embed, seq_len, -1)
+        prompt_embeds_list.append(prompt_embeds)
+
+    prompt_embeds = torch.concat(prompt_embeds_list, dim=-1)
+    pooled_prompt_embeds = pooled_prompt_embeds.view(bs_embed, -1)
+
+
+    return prompt_embeds, pooled_prompt_embeds
+
 def encode_prompts(prompts, tokenizers, text_encoders):
     """
     Used for SDXL text-encoder logic. 
